3c7ce7219fd15b53433b4cda9967128e72d1a1ec
Ashmin <ashmin2802@gmail.com>
2024-07-19 10:20:47 +0530

    ch

--- src/app/addnote/addnote.component.ts
+++ src/app/addnote/addnote.component.ts
@@ -9,6 +9,9 @@ import { BackendApiService } from '../services/backend-api.service';
 })
 export class AddnoteComponent {
 
+  inputValue: string = '';
+  InCout:Number=0
+  Letter:any
   constructor(private FB:FormBuilder,private Api:BackendApiService){}
 
   AddForm=this.FB.group({
@@ -32,6 +35,21 @@ export class AddnoteComponent {
 
   resetFm(){
     this.AddForm.reset()
+    this.InCout=0
+    this.Letter="Letter"
   }
+
+ 
+   InpLength(){
+    if(this.inputValue.length){
+      this.Letter="Letters"
+    }else{
+      this.Letter="Letter"
+    }
+    this.InCout=this.inputValue.length 
+  }
+  
+  
+
 
 }
--- src/app/addtodo/addtodo.component.ts
+++ src/app/addtodo/addtodo.component.ts
@@ -9,6 +9,10 @@ import { BackendApiService } from '../services/backend-api.service';
 })
 export class AddtodoComponent {
 
+  inputValue: string = '';
+  InCout:Number=0
+  Letter:any
+
   constructor(private FB:FormBuilder,private Api:BackendApiService){}
 
   AddTodoForm=this.FB.group({
@@ -29,6 +33,17 @@ export class AddtodoComponent {
 
   resetFm(){
     this.AddTodoForm.reset()
+    this.InCout=0
+    this.Letter="Letter"
+  }
+
+  InpLength(){
+    if(this.inputValue.length){
+      this.Letter="Letters"
+    }else{
+      this.Letter="Letter"
+    }
+    this.InCout=this.inputValue.length 
   }
 
 }
