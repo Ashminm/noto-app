581f44d3eee0549977cf9ef8b983381bf308e26a
Ashmin <ashmin2802@gmail.com>
2024-10-15 00:09:48 +0530

    complete

--- src/app/services/backend-api.service.ts
+++ src/app/services/backend-api.service.ts
@@ -8,8 +8,8 @@ import { BehaviorSubject } from 'rxjs';
 })
 export class BackendApiService {
 
-  SERVER_URL="http://localhost:3000"
-  // https://noto-server-lhnc.onrender.com
+  SERVER_URL="https://noto-server-lhnc.onrender.com"
+  // http://localhost:3000
   getHomeNote = new BehaviorSubject<any[]>([]);
   getHomeTodo = new BehaviorSubject<any[]>([]);
   getPrivetNote = new BehaviorSubject<any[]>([]);
